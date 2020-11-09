43fe7f02b38c01425ae88612dc3a495b31bc35ef
loqman <59010033+loqmanali@users.noreply.github.com>
2020-11-10 04:38:15 +0800

    add get all authors api call

---
+++ lib/api/authors_api.dart
@@ -0,0 +1,29 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
+import 'package:flutterappnews5/models/author.dart';
+import 'package:flutterappnews5/utilites/api_utilities.dart';
+
+class AuthorsAPI {
+  Future<List<Author>> fetchAllAuthors() async {
+    List<Author> authors = List<Author>();
+    String allAuthorsApi = base_api + all_authors_api;
+    var response = await http.get(allAuthorsApi);
+    if (response.statusCode == 200) {
+      var jsonData = jsonDecode(response.body);
+      var data = jsonData['date'];
+      print(data);
+
+      for (var item in data) {
+        Author author = Author(
+          item['id'].toString(),
+          item['name'].toString(),
+          item['email'].toString(),
+          item['avater'].toString(),
+        );
+        authors.add(author);
+      }
+    }
+    return authors;
+  }
+}
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutterappnews5/api/authors_api.dart';
 import 'package:flutterappnews5/screens/home_tabs/favourites.dart';
 import 'package:flutterappnews5/screens/home_tabs/popular.dart';
 import 'package:flutterappnews5/screens/home_tabs/whats_news.dart';
@@ -16,6 +17,8 @@ class _HomeScreenState extends State<HomeScreen>
     with SingleTickerProviderStateMixin {
   TabController _tabController;
 
+  AuthorsAPI authorsAPI = AuthorsAPI();
+
   @override
   void initState() {
     super.initState();
@@ -30,6 +33,7 @@ class _HomeScreenState extends State<HomeScreen>
 
   @override
   Widget build(BuildContext context) {
+    authorsAPI.fetchAllAuthors();
     return SafeArea(
       child: Scaffold(
         appBar: AppBar(
