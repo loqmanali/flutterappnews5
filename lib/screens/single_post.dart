07bd000fb442ed2b03330863446e370766642a67
loqman <59010033+loqmanali@users.noreply.github.com>
2020-11-13 17:27:43 +0800

    add comment text  entry

--- lib/screens/single_post.dart
+++ lib/screens/single_post.dart
@@ -13,41 +13,39 @@ class SinglePost extends StatefulWidget {
 class _SinglePostState extends State<SinglePost> {
   @override
   Widget build(BuildContext context) {
-    return CustomScrollView(
-      slivers: [
-        SliverAppBar(
-          expandedHeight: 300,
-          floating: true,
-          pinned: true,
-          flexibleSpace: FlexibleSpaceBar(
-            background: Container(
-              decoration: BoxDecoration(
-                image: DecorationImage(
-                    image: NetworkImage(widget.post.featureImage),
-                    fit: BoxFit.cover),
+    return Scaffold(
+      body: CustomScrollView(
+        slivers: [
+          SliverAppBar(
+            expandedHeight: 300,
+            floating: true,
+            pinned: true,
+            flexibleSpace: FlexibleSpaceBar(
+              background: Container(
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                      image: NetworkImage(widget.post.featureImage),
+                      fit: BoxFit.cover),
+                ),
               ),
             ),
           ),
-        ),
-        SliverList(
-          delegate: SliverChildBuilderDelegate(
-            (BuildContext context, position) {
-              if (position == 0) {
-                return Container(
-                  height: 500,
-                  color: Colors.amber,
-                );
-              } else {
-                return Container(
-                  height: 250,
-                  color: getRandomColor(),
-                );
-              }
-            },
-            childCount: 25,
+          SliverList(
+            delegate: SliverChildBuilderDelegate(
+              (BuildContext context, position) {
+                if (position == 0) {
+                  return _drawPostDetails();
+                } else if (position >= 1 && position < 24) {
+                  return _comments();
+                } else {
+                  return _commentTextEntry();
+                }
+              },
+              childCount: 25,
+            ),
           ),
-        ),
-      ],
+        ],
+      ),
     );
   }
 
@@ -59,6 +57,79 @@ class _SinglePostState extends State<SinglePost> {
       minBrightness + random.nextInt(255 - minBrightness),
       minBrightness + random.nextInt(255 - minBrightness),
       minBrightness + random.nextInt(255 - minBrightness),
+    );
+  }
+
+  Widget _drawPostDetails() {
+    return Container(
+      padding: EdgeInsets.all(16.0),
+      child: Text(
+        widget.post.content,
+        style: TextStyle(
+          fontSize: 18,
+          letterSpacing: 1.2,
+          height: 1.25,
+        ),
+      ),
+    );
+  }
+
+  Widget _comments() {
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: Column(
+        children: [
+          Row(
+            children: [
+              CircleAvatar(
+                backgroundImage: ExactAssetImage('assets/placeholder_bg.png'),
+              ),
+              SizedBox(width: 16),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text('loqman'),
+                  Text('1 hour'),
+                ],
+              ),
+            ],
+          ),
+          SizedBox(height: 16),
+          Text('datadatadatadatadatadatadatadatadatadatadatadatadatadatadata'),
+        ],
+      ),
+    );
+  }
+
+  Widget _commentTextEntry() {
+    return Container(
+      color: Color.fromRGBO(241, 245, 247, 1),
+      child: Column(
+        children: [
+          Row(
+            children: [
+              Flexible(
+                child: TextField(
+                  decoration: InputDecoration(
+                    border: InputBorder.none,
+                    hintText: 'Write Comment here',
+                    contentPadding:
+                        EdgeInsets.only(left: 16, top: 24, bottom: 28),
+                  ),
+                ),
+              ),
+              FlatButton(
+                onPressed: () {},
+                child: Text(
+                  'SEND',
+                  style: TextStyle(color: Colors.red),
+                ),
+              ),
+            ],
+          ),
+          SizedBox(height: 8.0),
+        ],
+      ),
     );
   }
 }
